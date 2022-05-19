be058e6432b6bf5b0cac8feb6dd5b0f723053e3e
Jeongwhale <wook971215@gmail.com>
2022-05-19 14:29:15 +0900

    add oncloth project v.1.0.0
    add oncloth project v.1.0.0

---
+++ oncloth/src/main/java/oncloth/oncloth.java
@@ -0,0 +1,4 @@
+package oncloth;
+
+public class oncloth {
+}
---
+++ oncloth/src/test/java/com/react/oncloth/OnclothApplicationTests.java
@@ -0,0 +1,13 @@
+package com.react.oncloth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnclothApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
