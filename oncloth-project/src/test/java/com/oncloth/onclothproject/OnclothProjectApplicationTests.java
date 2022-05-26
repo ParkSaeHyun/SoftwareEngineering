03508d94faaebcdd1883b2daefc63fb2a07e66ed
Jeongwhale <wook971215@gmail.com>
2022-05-26 10:31:55 +0900

    add classdiagram Ver 1.0.0, oncloth-project
    add classdiagram Ver 1.0.0, oncloth-project

---
+++ oncloth-project/src/test/java/com/oncloth/onclothproject/OnclothProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oncloth.onclothproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnclothProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
