6712cb384c4c1afb504373c9f2558f5b6a026d2b
rewritestar <rewritestar@naver.com>
2022-06-02 22:23:49 +0900

    cloth interface

---
+++ oncloth-project/src/main/java/com/oncloth/onclothproject/service/ClothService.java
@@ -0,0 +1,18 @@
+//package com.oncloth.onclothproject.service;
+//
+//import com.oncloth.onclothproject.model.Cloth;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.stereotype.Service;
+//
+//import java.util.List;
+//
+//@Service
+//public interface ClothService {
+//    public Long add(Cloth cloth);
+//    public Cloth check(Long id);
+//    //public Cloth modify(ModifyClothForm modifyClothForm);
+//    public Cloth delete(Long id);
+//    public List<Cloth> clothSearch(String keyword);
+//    //public List<Cloth> mainCategoryRead(MainCategoryForm mainCategoryForm);
+//    public List<Cloth> customCategoryRead(String customCategoryName);
+//}
---
+++ oncloth-project/src/main/java/com/oncloth/onclothproject/service/TrashbinService.java
@@ -0,0 +1,14 @@
+//package com.oncloth.onclothproject.service;
+//
+//import com.oncloth.onclothproject.model.Cloth;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.stereotype.Service;
+//
+//import java.util.List;
+//
+//@Service
+//public interface TrashbinService {
+//    public List<Cloth> trashbinRead();
+//    public Cloth trashbinRemove(Long clothId);
+//    public Cloth trashbinRestore(Long clothId);
+//}
