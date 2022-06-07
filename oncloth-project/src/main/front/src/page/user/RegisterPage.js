48a8f337216a355066389f95eb8863005d0492c4
Pridesd <whwotjr98@naver.com>
2022-06-07 17:24:57 +0900

    FormData

--- oncloth-project/src/main/front/src/page/user/RegisterPage.js
+++ oncloth-project/src/main/front/src/page/user/RegisterPage.js
@@ -4,21 +4,51 @@ import img from "../../base/소공로고.jpg";
 import Logo from "../../base/Logo";
 import axios from "axios";
 import { useNavigate } from "react-router";
+import { useState } from "react";
 
 const RegisterPage = () => {
 
-    const navigate = useNavigate();
-    const data = {
-        userid: "user1",
-        userpassword: "1234",
-        usernickname: "user1"
-    };
+    // const url = "/api/register";
+    // const sendParam = {
+    //     userid: "asdsad",
+    //     userpassword: "zxc",
+    //     usernickname: "asd"
+    // }
+    //
+    // axios.post(url, sendParam)
+    //     .then((res) => {
+    //         console.log(res)
+    //     })
+    //     .catch((error) => {
+    //         console.log(error.response)
+    //     })
 
+
+    const navigate = useNavigate();
+    const [inputId, setInputid] = useState("");
+    const [inputPw, setInputPw] = useState("");
+    const [inputNickName, setInputNickName] = useState("");
+
+    const onChangeId = (e) => {
+        setInputid(e.target.value);
+    }
+    const onChangePw = (e) => {
+        setInputPw(e.target.value);
+    }
+    const onChangeNickName = (e) => {
+        setInputNickName(e.target.value);
+    }
     const onSubmit = (e) => {
         e.preventDefault();
+        const data = {
+            userid: inputId,
+            userpassword: inputPw,
+            usernickname: inputNickName
+        };
         axios.post("/api/register/", JSON.stringify(data),{headers:{
                 "Content-Type":"application/json"
             }});
+        alert(`환영합니다! ${data.usernickname}님!`);
         navigate('/');
     };
 
@@ -30,9 +60,9 @@ const RegisterPage = () => {
                 <Logo />
                 <h2>회원가입</h2>
                 <form className="loginForm" onSubmit={onSubmit} action="">
-                    <input required type="text" id="id" placeholder="아이디"/>
-                    <input required type="text" id="nickname" placeholder="닉네임"/>
-                    <input required type="password" id="password" placeholder="비밀번호"/>
+                    <input required value={inputId} onChange={onChangeId} type="text" id="id" placeholder="아이디"/>
+                    <input required value={inputNickName} onChange={onChangeNickName} type="text" id="nickname" placeholder="닉네임"/>
+                    <input required value={inputPw} onChange={onChangePw} type="password" id="password" placeholder="비밀번호"/>
                     <Button type="submit">회원가입</Button>
                 </form>
             </div>
