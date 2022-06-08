d7dc02da78f855b46f4de1367a47783169a0cdd8
WooJin <tpfh801@gmail.com>
2022-06-08 21:29:33 +0900

    Category and ClothSearch

--- oncloth-project/src/main/java/com/oncloth/onclothproject/controller/ClothSearchController.java
+++ oncloth-project/src/main/java/com/oncloth/onclothproject/controller/ClothSearchController.java
@@ -4,12 +4,11 @@ import com.oncloth.onclothproject.model.Cloth;
 import com.oncloth.onclothproject.service.ClothSearchService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@Controller
+@RestController
 public class ClothSearchController {
     private final ClothSearchService clothSearchService;
 
@@ -18,8 +17,8 @@ public class ClothSearchController {
         this.clothSearchService = clothSearchService;
     }
 
-    @PostMapping("/api/search/")
-    public List<Cloth> clothSearch(@RequestBody String keyword) {
+    @GetMapping("/api/search/")
+    public List<Cloth> clothSearch(@RequestParam String keyword) {
         List<Cloth> clothList = clothSearchService.clothSearch(keyword);
         return clothList;
     }
