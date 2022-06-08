e010efb26b1c80f3fc7d11f66b7d58406c8638d9
WooJin <tpfh801@gmail.com>
2022-06-08 21:34:00 +0900

    Category and ClothSearch

---
+++ oncloth-project/src/main/resources/static/precache-manifest.4197d1bbc20bcff5d3e27a50351da216.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "f09823da65da936ef300",
+    "url": "/static/css/main.51a29076.chunk.css"
+  },
+  {
+    "revision": "f09823da65da936ef300",
+    "url": "/static/js/main.f09823da.chunk.js"
+  },
+  {
+    "revision": "c2f76bae2d092c75433e",
+    "url": "/static/js/1.c2f76bae.chunk.js"
+  },
+  {
+    "revision": "d39414d589da210ec76a",
+    "url": "/static/js/2.d39414d5.chunk.js"
+  },
+  {
+    "revision": "29880a8c837b3a72409b",
+    "url": "/static/js/runtime~main.29880a8c.js"
+  },
+  {
+    "revision": "4fa86328ce9920e4af004f65995fcc76",
+    "url": "/index.html"
+  }
+];
