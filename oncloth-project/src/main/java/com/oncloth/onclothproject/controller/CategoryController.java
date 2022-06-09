07a762c2b67b07e21c4a3fb4f5938343e737d54a
unknown <tpfh801@gmail.com>
2022-06-09 18:25:27 +0900

    readCustomCategoryList

--- oncloth-project/src/main/java/com/oncloth/onclothproject/controller/CategoryController.java
+++ oncloth-project/src/main/java/com/oncloth/onclothproject/controller/CategoryController.java
@@ -1,6 +1,5 @@
 package com.oncloth.onclothproject.controller;
 
-import com.oncloth.onclothproject.dto.ClothDto;
 import com.oncloth.onclothproject.dto.CustomCategoryDto;
 import com.oncloth.onclothproject.model.Cloth;
 import com.oncloth.onclothproject.model.CustomCategory;
@@ -42,7 +41,12 @@ public class CategoryController {
 
     @GetMapping("/api/customcategory/{id}")
     public List<Cloth> readCustomCategory(@PathVariable("id") Long id) {
-        List<Cloth> clothList = categoryService.customCategoryRead(id);
+        List<Cloth> clothList = categoryService.readCustomCategory(id);
         return clothList;
+    }
+
+    @GetMapping("/api/customcategory/list/")
+    public List<CustomCategory> readCustomCategoryList() {
+        return categoryService.readCustomCategoryList();
     }
 }
