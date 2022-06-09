8e9b29d1a528ce396fd3bc1847e5129a00176419
rewritestar <rewritestar@naver.com>
2022-06-09 15:30:35 +0900

    비번찾기수정, 헌옷수거함 수정

---
+++ oncloth-project/src/main/java/com/oncloth/onclothproject/model/FindpwForm.java
@@ -0,0 +1,10 @@
+package com.oncloth.onclothproject.model;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class FindpwForm {
+    private String userid;
+    private String usernickname;
+}
